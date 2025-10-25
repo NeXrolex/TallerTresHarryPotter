847a339915e2f6b3545722cbc7afe77c2d0426bf
santi <santi@DESKTOP-LU7UNG7>
2025-10-24 22:42:45 -0500

    Inicio

---
+++ src/com/uDistrital/avanzada/tallerTres/modelo/Persona.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.uDistrital.avanzada.tallerTres.modelo;
+
+/**
+ * Representa una persona en el sistema
+ *
+ * @author Alex
+ */
+public class Persona {
+    
+    private String nombre;
+    
+}
