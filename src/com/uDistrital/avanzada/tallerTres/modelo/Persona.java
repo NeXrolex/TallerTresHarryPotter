ff52a7e046e6644bacd7ffda0b15a3f6ef861863
santi <santi@DESKTOP-LU7UNG7>
2025-10-25 08:55:17 -0500

    Clase Persona

--- src/com/uDistrital/avanzada/tallerTres/modelo/Persona.java
+++ src/com/uDistrital/avanzada/tallerTres/modelo/Persona.java
@@ -13,4 +13,35 @@ public class Persona {
     
     private String nombre;
     
+    /**
+     * Constructor que asigna el nombre de una persona
+     * 
+     * @param nombre Nombre de la persona
+     */
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    /**
+     * Metodo que obtiene el nombre de la persona
+     * 
+     * @return Nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+    
+    /**
+     * Asigna el nombre de una persona
+     * 
+     * @param nombre Nombre de la perosna
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+    
+    
+    
 }
