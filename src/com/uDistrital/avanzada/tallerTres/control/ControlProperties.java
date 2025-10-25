6cc26ab2832ece8fc521f55b322fd3b7f3247ccd
santi <santi@DESKTOP-LU7UNG7>
2025-10-25 12:24:53 -0500

    Controles

--- src/com/uDistrital/avanzada/tallerTres/control/ControlProperties.java
+++ src/com/uDistrital/avanzada/tallerTres/control/ControlProperties.java
@@ -5,18 +5,11 @@
 package com.uDistrital.avanzada.tallerTres.control;
 
 /**
- * Lanza el programa
+ * Maneja toda la informacion de los arhivos de propiedades
+ * y aclara toda la logica del mismo
  *
  * @author Alex
  */
-public class Launcher {
-    
-    public static void main(String[] arg){
-        //Para las buenas practicas esto es lo unico que debe existir en 
-        //el launcher
-        new ControlGeneral();
-        
-        
-    }
+public class ControlProperties {
     
 }
