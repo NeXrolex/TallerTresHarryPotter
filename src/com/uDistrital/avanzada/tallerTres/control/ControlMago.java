ba93d36e169af9fb594416f839746dabacfb9f44
Steven Londoño <stlondonoh@udistrital.edu.co>
2025-10-28 16:19:35 -0500

    simulacion batalla

--- src/com/uDistrital/avanzada/tallerTres/control/ControlMago.java
+++ src/com/uDistrital/avanzada/tallerTres/control/ControlMago.java
@@ -4,27 +4,72 @@
  */
 package com.uDistrital.avanzada.tallerTres.control;
 
+import com.uDistrital.avanzada.tallerTres.modelo.Mago;
+import java.util.Random;
+
 /**
- * Maneja el control de los magos y ayuda al proceso de hilos 
- * para la concurrencia
+ * Maneja el control de los magos y ayuda al proceso de hilos para la
+ * concurrencia
  *
  * @author Alex
  * @version 1.0
  */
 public class ControlMago extends Thread {
-    
+
     private ControlGeneral cGeneral;
-    
+    private Mago mago;
+    private Mago rival;
+    private Random random = new Random();
+    private Object lock;
+
     /**
-     * Recibe la inyeccion del control General para respetar el 
-     * bajo acoplamiento
-     * 
+     * Recibe la inyeccion del control General para respetar el bajo
+     * acoplamiento
+     *
      * @param cGeneral Control General
      */
-    public ControlMago(ControlGeneral cGeneral) {
+    public ControlMago(ControlGeneral cGeneral, Object lock) {
         this.cGeneral = cGeneral;
+        this.lock = lock;
     }
-    
-    
-    
+
+    public void setMagos(Mago mago, Mago rival) {
+        this.mago = mago;
+        this.rival = rival;
+    }
+
+    @Override
+    public void run() {
+        while (mago.getPuntaje() < 250 && rival.getPuntaje() < 250) {
+            synchronized (lock) {
+                try {
+                    // Esperar si el mago está aturdido
+                    if (mago.isEstadoAturdido()) {
+                        lock.wait(random.nextInt(250) + 1);
+                        mago.setEstado(false);
+                        continue;
+                    }
+
+                    // Generar hechizo simulado (entre 5 y 25 puntos)
+                    int puntosHechizo = random.nextInt(21) + 5;
+                    mago.setPuntaje(mago.getPuntaje() + puntosHechizo);
+                    mago.setCantidadHechizos(mago.getCantidadHechizos() + 1);
+
+                    // Simular que el rival queda aturdido
+                    rival.setEstado(true);
+
+                    // Espera antes del siguiente turno
+                    lock.notifyAll();
+                    lock.wait(random.nextInt(500) + 1);
+
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+            }
+        }
+        synchronized (lock) {
+            lock.notifyAll(); // Asegura que el otro hilo no quede bloqueado
+        }
+    }
 }
--- src/com/uDistrital/avanzada/tallerTres/modelo/Mago.java
+++ src/com/uDistrital/avanzada/tallerTres/modelo/Mago.java
@@ -11,96 +11,100 @@ package com.uDistrital.avanzada.tallerTres.modelo;
  * @version 1.0
  */
 public class Mago extends Persona {
-    
+
     private String casa;
     private int cantidadHechizos;
     private int puntaje;
     private boolean estadoAturdido;
-    
+
     /**
      * Constructor que asigna atriutos del mago
-     * 
+     *
      * @param nombre Nombre del mago
      * @param casa Casa del mago
      */
     public Mago(String nombre, String casa) {
         super(nombre);
         this.casa = casa;
-        
+
     }
-    
+
     /**
      * Retorna el valor de la casa
-     * 
+     *
      * @return Casa
      */
     public String getCasa() {
         return casa;
     }
-    
+
     /**
      * Asigna el valor de la casa
-     * 
+     *
      * @param casa Casa del mago
      */
     public void setCasa(String casa) {
         this.casa = casa;
     }
-    
+
     /**
      * retorna el valor del puntaje
-     * 
+     *
      * @return Puntaje
      */
     public int getPuntaje() {
         return puntaje;
     }
-    
+
     /**
      * Asigna el valor del puntaje
-     * 
-     * @param puntaje Puntaje del mago 
+     *
+     * @param puntaje Puntaje del mago
      */
     public void setPuntaje(int puntaje) {
         this.puntaje = puntaje;
     }
-    
+
     /**
-     * Obtiene la cantidad de hechizos 
-     * 
+     * Obtiene la cantidad de hechizos
+     *
      * @return cantidad de hechizos
      */
     public int getCantidadHechizos() {
         return cantidadHechizos;
     }
-    
+
     /**
      * Asigna la cantidad de hechizos
-     * 
+     *
      * @param cantidadHechizos Cantidad de hechizos del mago
      */
     public void setCantidadHechizos(int cantidadHechizos) {
         this.cantidadHechizos = cantidadHechizos;
     }
-    
+
     /**
      * En que estado se encuentra el jugador
-     * 
+     *
      * @return True or false para los casos
      */
     public boolean isEstadoAturdido() {
         return estadoAturdido;
     }
-    
+
     /**
      * Asigna el estado del mago
-     * 
+     *
      * @param estadoAturdido Estado actual del mago
      */
     public void setEstado(boolean estadoAturdido) {
         this.estadoAturdido = estadoAturdido;
     }
-    
-    
-    
+
+    public void resetPuntaje() {
+        this.puntaje = 0;
+        this.cantidadHechizos = 0;
+        this.estadoAturdido = false;
+    }
+
 }
